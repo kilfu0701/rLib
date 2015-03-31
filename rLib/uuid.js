96afb8e14d9472e600d90299ba9d7e7177a55758
kilfu0701 <kilfu0701@gmail.com>
2015-03-31 16:00:33 +0800

    Update uuid.js code.

--- rLib/uuid.js
+++ rLib/uuid.js
@@ -1,9 +1,9 @@
-function uuid(str) {
-    str = str || 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx';
+'use strict';
 
-    return str.replace(/[xy]/g, function(c) { var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);  return v.toString(16); });
-};
+let uuid = (str = 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx') =>
+    str.replace(/[xy]/g, (c) => {
+        let r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
 
-if(typeof exports !== 'undefined') {
-    exports.uuid = uuid;
-}
+exports.uuid = uuid;
