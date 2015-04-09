4b30f9e27961b9370b8c4dea5ea2676f4c34b764
kilfu0701 <kilfu0701@gmail.com>
2015-04-09 17:07:51 +0800

    Add util/Object.js.

---
+++ rLib/util/Object.js
@@ -0,0 +1,24 @@
+/**
+ * @description
+ *      Extend more functions for Object.
+ *
+ */
+
+Object.extend = function(destination, source) {
+    for (var property in source) {
+        if (source[property] && source[property].constructor &&
+            source[property].constructor === Object)
+        {
+            destination[property] = destination[property] || {};
+            arguments.callee(destination[property], source[property]);
+        } else {
+            destination[property] = source[property];
+        }
+    }
+
+    return destination;
+};
+
+console.log('Object.extend =', Object.extend);
+
+exports.Object = Object;
